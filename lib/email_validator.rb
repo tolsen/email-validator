fc5e1e934bc38aa5c806ffa44462cebe74d70737
Tim Olsen <tim@jux.com>
2011-09-23 10:48:26 -0400

    match beginning and end of string, not line in EMAIL_RX

--- lib/email_validator.rb
+++ lib/email_validator.rb
@@ -41,7 +41,7 @@ module EmailValidator
 
     EMAIL = "#{LENGTH_CHECK}#{LOCAL_PART_DOT_ATOM}\@#{DOMAIN}"
 
-    EMAIL_RX = /^#{EMAIL}$/
+    EMAIL_RX = /\A#{EMAIL}\z/
   end
   
   def self.valid_email?(email) !!EMAIL_RX.match(email); end
